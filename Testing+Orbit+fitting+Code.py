71544ea52632c4adc32d104edae1b41045435479
Lynus <lynusz@umich.edu>
2016-07-08 03:28:54 -0400

    second commit

---
+++ Testing+Orbit+fitting+Code.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# import the package:
+from pyOrbfit.Orbit import Orbit
+import ephem 
+import numpy as np
+
+
+# In[ ]:
+
+
+o = Orbit(file='exposures.csv')
+
+
+# In[ ]:
+
+
+
